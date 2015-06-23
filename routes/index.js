70b7464fcb41188a1e57f467b93971f93ead70c7
astronomacat <florencio.crespo@gmail.com>
2015-06-23 14:00:14 +0200

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -7,8 +7,11 @@ var quizController = require('../controllers/quiz_controller');
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Quiz' });
 });
-
+router.get('/autor', function(req, res, next) {
+  res.render('autor', { title: 'Quiz' });
+});
 router.get('/quizes/question', quizController.question);
 router.get('/quizes/answer', quizController.answer);
+
 
 module.exports = router;
