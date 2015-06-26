5bf4a8dbb82abba448e61f824a4e971f160bb9b4
florenciocrespo <florencio.crespo@gmail.com>
2015-06-26 13:16:07 +0200

    BD

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,3 +1,5 @@
+var models = require('../models/models.js');
+/*
 // GET /quizes/question
 exports.question = function(req, res) {
 	res.render('quizes/question', {pregunta: 'Capital de Italia'});
@@ -10,4 +12,30 @@ exports.answer = function(req, res) {
 	} else {
 		res.render('quizes/answer', {respuesta: 'Incorrecto'});
 	}
+};
+
+*/
+
+// findAll(quiz) devuelve todos los objetos de la tabla
+// como solo hay una pregunta, está será quiz[0] 
+exports.question = function(req, res){
+  models.Quiz.findAll().success(function(quiz) {
+    res.render('quizes/question', { pregunta: quiz[0].pregunta});
+  })
+};
+
+exports.answer = function(req, res){
+  models.Quiz.findAll().success(function(quiz) {
+    if (req.query.respuesta === quiz[0].respuesta) {
+      res.render('quizes/answer', { respuesta: 'Correcto' });
+    } else {
+      res.render('quizes/answer', { respuesta: 'La respuesta es: ' + quiz[0].respuesta});
+    }
+  })
+};
+
+// GET /author
+
+exports.autor = function(req, res) {
+      res.render('autor', {title:'Quiz', autor: 'Florencio Crespo'});
 };
--- routes/index.js
+++ routes/index.js
@@ -7,11 +7,9 @@ var quizController = require('../controllers/quiz_controller');
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Quiz' });
 });
-router.get('/autor', function(req, res, next) {
-  res.render('autor', { title: 'Quiz' });
-});
+
 router.get('/quizes/question', quizController.question);
 router.get('/quizes/answer', quizController.answer);
-
+router.get('/autor', quizController.autor);
 
 module.exports = router;
