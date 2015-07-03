94406ef493420aa8f3c9d2d54c18d5ac65e5e2a6
florenciocrespo <florencio.crespo@gmail.com>
2015-07-03 12:23:57 +0200

    fin tema 7

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var bodyParser = require('body-parser');
 var partials = require('express-partials');
 
 var routes = require('./routes/index');
+var methodOverride = require('method-override');
 
 
 var app = express();
@@ -20,8 +21,10 @@ app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(partials());
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+//app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded());
 app.use(cookieParser());
+app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
@@ -43,7 +46,8 @@ if (app.get('env') === 'development') {
     res.status(err.status || 500);
     res.render('error', {
       message: err.message,
-      error: err
+      error: err,
+      errors: []
     });
   });
 }
@@ -54,7 +58,8 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error', {
     message: err.message,
-    error: {}
+    error: {},
+    errors: []
   });
 });
 
--- routes/index.js
+++ routes/index.js
@@ -5,14 +5,20 @@ var quizController = require('../controllers/quiz_controller');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Quiz' });
+  res.render('index', { title: 'QUIZ', errors: [] });
 });
-//auto
+// Autoload de comandos con :quizId
 router.param('quizId', quizController.load);
 
+// Definición de rutas de /quizes
 router.get('/quizes',                      quizController.index);
 router.get('/quizes/:quizId(\\d+)',        quizController.show);
 router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
-
+router.get('/quizes/new',                  quizController.new);
+router.post('/quizes/create',              quizController.create);
+router.get('/quizes/:quizId(\\d+)/edit',   quizController.edit);
+router.put('/quizes/:quizId(\\d+)',        quizController.update);
+router.delete('/quizes/:quizId(\\d+)',     quizController.destroy);
+router.get('/autor',                       quizController.autor);
 
 module.exports = router;
